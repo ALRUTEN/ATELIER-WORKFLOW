c1ae8f03522e4088985873888c9e8686275440a1
ALRUTEN <laurentnavarro31@gmail.com>
2017-11-06 14:17:20 +0100

    map

--- app/src/main/java/com/example/apprenti/atelierworkflow/EmptyActivity.java
+++ app/src/main/java/com/example/apprenti/atelierworkflow/EmptyActivity.java
@@ -1,8 +1,11 @@
 package com.example.apprenti.atelierworkflow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -12,12 +15,21 @@ import com.google.firebase.database.ValueEventListener;
 
 public class EmptyActivity extends AppCompatActivity {
 
+    Button map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty);
 
-
+        Button map = (Button) findViewById(R.id.map);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EmptyActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
